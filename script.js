aea158c4abbcfa909c51605089c8597eb79aba51
Nexus <124392432+Nexus-Programmer@users.noreply.github.com>
2025-03-28 02:16:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// If you know HTML, CSS and JavaScript well then optimize this JavaScript file and add better functions. (Recommended :D)
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
 
